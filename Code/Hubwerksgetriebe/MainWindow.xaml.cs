bd302a9a805f091c1bc5a61a2c9c49b117023c6a
danielweindl <danielweindl@DANIELWEIND0691>
2025-12-18 14:25:16 +0100

    #4 - Drehung implementiert

--- Code/Hubwerksgetriebe/MainWindow.xaml.cs
+++ Code/Hubwerksgetriebe/MainWindow.xaml.cs
@@ -17,13 +17,39 @@ namespace Hubwerksgetriebe
     /// </summary>
     public partial class MainWindow : Window
     {
+        #region PRIVATE VARIABLES
         private float _rotation = 0;
+        // Rollenparameter
+        private double _r2 = 1;
+        private double _r32 = 3;
+        private double _r34 = 1.5;
+        private double _r43 = 2.5;
+        private double _r45 = 1.5;
+        private double _thickness = 1;
+        private int _segments = 60;
+        // X & Z Positionen
+        private double _x2 = 0;
+        private double _x3 = 4;
+        private double _x4 = 8;
+        private double _z3 = 1;
+        private double _z45 = 2;
+        // Winkel
+        private double _phi2 = 0.0; // [rad]
 
+        #endregion
+        
         public MainWindow()
         {
             InitializeComponent();
         }
-
+        
+        public enum DiskMarkerColor
+        {
+            Red,
+            Green,
+            Blue
+        }
+        
         private void OpenGLControl_OpenGLInitialized(object sender, SharpGL.WPF.OpenGLRoutedEventArgs args)
         {
             OpenGL gl = args.OpenGL;
@@ -50,88 +76,36 @@ namespace Hubwerksgetriebe
         private void OpenGLControl_OpenGLDraw(object sender, SharpGL.WPF.OpenGLRoutedEventArgs args)
         {
             OpenGL gl = args.OpenGL;
-
+            double phi3 = _phi2 * (_r2 / _r32);
+            double phi4 = phi3 * (_r32 / _r43);
+            
             gl.Clear(OpenGL.GL_COLOR_BUFFER_BIT | OpenGL.GL_DEPTH_BUFFER_BIT);
             gl.LoadIdentity();
-
+            
+            // Kamera ausrichtung
             gl.Translate(0, 0, -25);
-            //gl.Rotate(_rotation, 0, 1, 0);
-            gl.Rotate(-30,0,1,0);
-            gl.Rotate(-10,0,0,1);
-            // ---------- Fadenkreuz ----------
-            gl.Disable(OpenGL.GL_LIGHTING);
-
-            gl.Color(0.0f, 0.0f, 0.0f);
-            gl.LineWidth(2.0f);
-
-            gl.Begin(OpenGL.GL_LINES);
-            
-            // X-Achse
-            gl.Vertex(-10.0f, 0.0f, 0.0f);
-            gl.Vertex(10.0f, 0.0f, 0.0f);
-
-            // Y-Achse
-            gl.Vertex(0.0f, -10.0f, 0.0f);
-            gl.Vertex(0.0f, 10.0f, 0.0f);
-
-            // Z-Achse (in die Tiefe)
-            gl.Vertex(0.0f, 0.0f, -10.0f);
-            gl.Vertex(0.0f, 0.0f, 10.0f);
-            gl.End();
-            
-            // ---------- Achsenbeschriftung ----------
-            gl.DrawText(
-                10, 10,              // Screen-Koordinaten (px)
-                0.0f, 0.0f, 0.0f,    // Farbe
-                "Arial",
-                12,
-                "X →"
-            );
-            gl.DrawText(
-                10, 30,
-                0.0f, 0.0f, 0.0f,
-                "Arial",
-                12,
-                "Y ↑"
-            );
-            gl.DrawText(
-                10, 50,
-                0.0f, 0.0f, 0.0f,
-                "Arial",
-                12,
-                "Z ⊙"
-            );
-
+            //gl.Rotate(20, 1, 0, 0);
+            //gl.Rotate(-40, 0, 1, 0);
+            DrawFadenkreuz(gl);
+             gl.Rotate(_rotation, 0, 1, 0);
             gl.Enable(OpenGL.GL_LIGHTING);
-            // ---------------- Rollenparameter ----------------
-            double r2 = 1;
-            double r32 = 3;
-            double r34 = 1.5;
-            double r43 = 2.5;
-            double r45 = 1.5;
             
-            double thickness = 1;
-            int segments = 60;
-            double x2 = 0;
-            double x3 = 4; 
-            double x4 = 8;
-
-            double z3 = 1;
-            double z45 = 2;
             // ---------------- Rolle 1 ----------------
             SetMaterial(gl, 0.6f, 0.6f, 0.6f);
-            DrawDiskXY(gl, r2, thickness, segments, x2, 0, 0);
-        
+            DrawDiskXy(gl, _r2, _thickness, _segments, _x2, 0, 0);
+            DrawDiskMarkerXy(gl, _r2, _thickness, 0,0, DiskMarkerColor.Red);
+            
             // ---------------- Rolle 2 ----------------
             SetMaterial(gl, 0.4f, 0.6f, 0.9f);
-            DrawDiskXY(gl, r32, thickness, segments, x3, 0, 0);
-            DrawDiskXY(gl, r34, thickness, segments, x3, 0, z3);
-            
+            DrawDiskXy(gl, _r32, _thickness, _segments, _x3, 0, 0);
+            DrawDiskXy(gl, _r34, _thickness, _segments, _x3, 0, _z3);
+            DrawDiskMarkerXy(gl, _r32, _thickness, 0,0, DiskMarkerColor.Green);
+            DrawDiskMarkerXy(gl, _r34, _thickness, _r32,2, DiskMarkerColor.Blue);
             // ---------------- Rolle 3 ----------------
             SetMaterial(gl, 0.7f, 0.4f, 0.4f);
-            DrawDiskXY(gl, r43, thickness, segments, x4, 0, z3);
-            DrawDiskXY(gl, r45, thickness, segments, x4, 0, z45);
-
+            DrawDiskXy(gl, _r43, _thickness, _segments, _x4, 0, _z3);
+            DrawDiskXy(gl, _r45, _thickness, _segments, _x4, 0, _z45);
+            //DrawDiskMarkerXy(gl, _r43, _thickness, 0,0, DiskMarkerColor.Blue);
             // ---------------- Seil ----------------
             gl.Disable(OpenGL.GL_LIGHTING);
             gl.Color(0, 0, 0);
@@ -139,19 +113,13 @@ namespace Hubwerksgetriebe
 
             double ropeTopY = 0;
             double ropeBottomY = -4.5;
-
-            // gl.Begin(OpenGL.GL_LINES);
-            // gl.Vertex(9.5, ropeTopY, 0);
-            // gl.Vertex(9.5, ropeBottomY, 0);
-            // gl.End();
-
-            double ropeZ = 2;   // Verschiebung in Z-Richtung
+            
+            double ropeZ = 2; // Verschiebung in Z-Richtung
 
             gl.Begin(OpenGL.GL_LINES);
             gl.Vertex(9.5, ropeTopY, ropeZ);
             gl.Vertex(9.5, ropeBottomY, ropeZ);
             gl.End();
-            
             
             gl.Enable(OpenGL.GL_LIGHTING);
 
@@ -160,15 +128,14 @@ namespace Hubwerksgetriebe
             DrawSphere(gl, 0.5, 30, 30, 9.5, ropeBottomY - 0.4, 2);
 
             _rotation += 0.5f;
+            _phi2 += 0.01; // rad pro Frame
         }
 
         // ================= Hilfsmethoden =================
-
-        private void DrawDiskXY(OpenGL gl, double radius, double thickness, int segments,
-            double cx, double cy, double cz)
+        private static void DrawDiskXy(OpenGL gl, double radius, double thickness, int segments, double cx, double cy, double cz)
         {
             double zFront = cz + thickness / 2.0;
-            double zBack  = cz - thickness / 2.0;
+            double zBack = cz - thickness / 2.0;
 
             // ---------- Vorderfläche (+Z) ----------
             gl.Begin(OpenGL.GL_TRIANGLE_FAN);
@@ -183,6 +150,7 @@ namespace Hubwerksgetriebe
 
                 gl.Vertex(cx + x, cy + y, zFront);
             }
+
             gl.End();
 
             // ---------- Rückfläche (-Z) ----------
@@ -198,6 +166,7 @@ namespace Hubwerksgetriebe
 
                 gl.Vertex(cx + x, cy + y, zBack);
             }
+
             gl.End();
 
             // ---------- Mantel ----------
@@ -214,8 +183,37 @@ namespace Hubwerksgetriebe
             }
             gl.End();
         }
-        
-        private void DrawSphere(OpenGL gl, double radius, int slices, int stacks, double cx, double cy, double cz)
+        private void SetMarkerColor(OpenGL gl, DiskMarkerColor color)
+        {
+            switch (color)
+            {
+                case DiskMarkerColor.Red:
+                    gl.Color(1.0f, 0.0f, 0.0f);
+                    break;
+
+                case DiskMarkerColor.Green:
+                    gl.Color(0.0f, 1.0f, 0.0f);
+                    break;
+
+                case DiskMarkerColor.Blue:
+                    gl.Color(0.0f, 0.0f, 1.0f);
+                    break;
+            }
+        }
+        private void DrawDiskMarkerXy(OpenGL gl, double radius, double thickness, double xOffset, double zOffset, DiskMarkerColor color)
+        {
+            double z = (thickness/2 + 0.1) +  zOffset;
+            gl.Disable(OpenGL.GL_LIGHTING);
+            gl.LineWidth(3.0f);
+            SetMarkerColor(gl, color);
+            gl.Begin(OpenGL.GL_LINES);
+            gl.Vertex(xOffset, 0.0, z);
+            gl.Vertex(radius + xOffset, 0.0, z);
+            gl.End();
+
+            gl.Enable(OpenGL.GL_LIGHTING);
+        }
+        private static void DrawSphere(OpenGL gl, double radius, int slices, int stacks, double cx, double cy, double cz)
         {
             for (int j = 0; j < stacks; j++)
             {
@@ -241,11 +239,11 @@ namespace Hubwerksgetriebe
                     gl.Normal(x2, y2, z2);
                     gl.Vertex(cx + radius * x2, cy + radius * y2, cz + radius * z2);
                 }
+
                 gl.End();
             }
         }
-
-        private void SetMaterial(OpenGL gl, float r, float g, float b)
+        private static void SetMaterial(OpenGL gl, float r, float g, float b)
         {
             float[] amb = { 0.2f, 0.2f, 0.2f, 1 };
             float[] diff = { r, g, b, 1 };
@@ -256,6 +254,52 @@ namespace Hubwerksgetriebe
             gl.Material(OpenGL.GL_FRONT_AND_BACK, OpenGL.GL_DIFFUSE, diff);
             gl.Material(OpenGL.GL_FRONT_AND_BACK, OpenGL.GL_SPECULAR, spec);
             gl.Material(OpenGL.GL_FRONT_AND_BACK, OpenGL.GL_SHININESS, shin);
+        }
+        private static void DrawFadenkreuz(OpenGL gl)
+        {
+            // ---------- Fadenkreuz ----------
+            gl.Disable(OpenGL.GL_LIGHTING);
+
+            gl.Color(0.0f, 0.0f, 0.0f);
+            gl.LineWidth(2.0f);
+
+            gl.Begin(OpenGL.GL_LINES);
+
+            // X-Achse
+            gl.Vertex(-10.0f, 0.0f, 0.0f);
+            gl.Vertex(10.0f, 0.0f, 0.0f);
+
+            // Y-Achse
+            gl.Vertex(0.0f, -10.0f, 0.0f);
+            gl.Vertex(0.0f, 10.0f, 0.0f);
+
+            // Z-Achse (in die Tiefe)
+            gl.Vertex(0.0f, 0.0f, -10.0f);
+            gl.Vertex(0.0f, 0.0f, 10.0f);
+            gl.End();
+
+            // ---------- Achsenbeschriftung ----------
+            gl.DrawText(
+                10, 10, // Screen-Koordinaten (px)
+                0.0f, 0.0f, 0.0f, // Farbe
+                "Arial",
+                12,
+                "X →"
+            );
+            gl.DrawText(
+                10, 30,
+                0.0f, 0.0f, 0.0f,
+                "Arial",
+                12,
+                "Y ↑"
+            );
+            gl.DrawText(
+                10, 50,
+                0.0f, 0.0f, 0.0f,
+                "Arial",
+                12,
+                "Z ⊙"
+            );
         }
     }
 }    
